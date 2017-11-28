426d441ea5239d6fb0ab566d7d1a7180b934819d
Melissa Mepham <m.a.mepham@gmail.com>
2017-11-28 19:49:02 +1300

    done - not pretty

--- main.js
+++ main.js
@@ -6,31 +6,13 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://eda-te-reo.herokuapp.com/api/whakatauki");
 xhr.onload = function printText() {
   var proverbString = JSON.parse(xhr.responseText);
-  proverbHtmlMaori(proverbString['source']);
-  proverbHtmlEnglish(proverbString['translation']);
+  proverbHtmlMaori(proverbString['source'], proverbString['translation'])
 };
 xhr.send();
 });
 
-function proverbHtmlMaori (maori) {
+function proverbHtmlMaori (maori, english) {
   var htmlStringMaori = ""
-    htmlStringMaori += "<p>" + maori + "</p>";
-  proverbContainer.insertAdjacentHTML('beforeend', htmlStringMaori)
+    htmlStringMaori += "<p>" + maori + "</p>" + "<p>" + english + "</p>";
+  proverbContainer.innerHTML = htmlStringMaori
 }
-
-function proverbHtmlEnglish (english) {
-  var htmlStringEnglish = ""
-    htmlStringEnglish += "<p>" + english + "</p>";
-  proverbContainer.insertAdjacentHTML('beforeend', htmlStringEnglish)
-}
-
-var buttonCount = document.getElementById('myButton'),
-  count = 0;
-buttonCount.onclick = function() {
-  count ++;
-if (count > 1) {
-  location.reload();
-  proverbHtmlMaori(proverbString['source']);
-  proverbHtmlEnglish(proverbString['translation']);
-    }
-  }
